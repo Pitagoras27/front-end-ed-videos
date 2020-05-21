5413ff80c2620751ae307eb753443bf28dc4142c
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-05-20 21:27:27 -0500

    Se conecta carouselItem con connect y se genera action y reducer

--- src/components/CarouselItem/CarouselItem.jsx
+++ src/components/CarouselItem/CarouselItem.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import setFavorite from '../../actions';
 import '../../assets/styles/components/CarouselItem.scss';
 import playIcon from '../../assets/static/play-icon.png';
 import plusIcon from '../../assets/static/plus-icon.png';
 
-const CarouselItem = ({ cover, description, year, contentRating, duration }) => (
-  <div className='carousel-item'>
-    <img
-      className='carousel-item__img'
-      src={cover}
-      alt='People'
-    />
-    <div className='carousel-item__details'>
-      <div>
-        <img src={playIcon} alt='Play' />
-        <img src={plusIcon} alt='Plus' />
+const CarouselItem = (props) => {
+  const { cover, description, year, contentRating, duration } = props;
+  const handleSetFavorite = () => {
+    props.setFavorite({
+      cover, description, year, contentRating, duration,
+    });
+  };
+
+  return (
+    <div className='carousel-item'>
+      <img
+        className='carousel-item__img'
+        src={cover}
+        alt='People'
+      />
+      <div className='carousel-item__details'>
+        <div>
+          <img src={playIcon} alt='Play' />
+          <img
+            src={plusIcon}
+            alt='Plus'
+            onClick={handleSetFavorite}
+          />
+        </div>
+        <p className='carousel-item__details--title'>
+          {description}
+        </p>
+        <p className='carousel-item__details--subtitle'>
+          {`${year} ${contentRating} ${duration}`}
+        </p>
       </div>
-      <p className='carousel-item__details--title'>
-        {description}
-      </p>
-      <p className='carousel-item__details--subtitle'>
-        {`${year} ${contentRating} ${duration}`}
-      </p>
     </div>
-  </div>
-);
+  );
+};
 
 CarouselItem.propTypes = {
   cover: PropTypes.string,
@@ -42,4 +58,8 @@ CarouselItem.defaultProps = {
   duration: 0,
 };
 
-export default CarouselItem;
+const mapDispatchToProps = {
+  setFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarouselItem);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,16 @@
-const reducer = (state, action) => state;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_FAVORITE':
+      return {
+        ...state,
+        mylist: [
+          ...state.mylist,
+          action.payload,
+        ],
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
