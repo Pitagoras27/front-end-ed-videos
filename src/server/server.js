c0d146a688b8f854caf7b4aaadb1cfcdaf8c0e89
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-05-29 17:13:29 -0500

    Configuracion de chunk vendorfiles

--- src/server/server.js
+++ src/server/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('Development config');
-  const webpackConfig = require('../../webpack.config.dev');
+  const webpackConfig = require('../../webpack.config');
   const webpackDevMiddleware = require('webpack-dev-middleware')
   const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(webpackConfig);
@@ -43,6 +43,7 @@ if (ENV === 'development') {
 const setResponse = (html, preloadedState, manifest) => {
   const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
   const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+  const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
 
   return (`
     <!DOCTYPE html>
@@ -59,6 +60,7 @@ const setResponse = (html, preloadedState, manifest) => {
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
         <script src="${mainBuild}" type="text/javascript"></script>
+        <script src="${vendorBuild}" type="text/javascript"></script>
         </body>
       </html>
     `
